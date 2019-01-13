0c1434e279a1d9eb256b86b8d1d3e122e74f1062
Stef Claus <stef.claus@gmail.com>
2019-01-13 15:20:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -40,18 +40,25 @@ class Student
     end
 
     def update
+      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
+    def self.create(name, grade)
+       student = Student.new(name, grade)
+       student.save
+       student
+     end
 
-    def self.create
-    end
 
     def self.new_from_db
     end
 
-    def self.find_by_name
-    end
-
+    def self.find_by_name(name)
+       sql = "SELECT * FROM students WHERE name = ?"
+       result = DB[:conn].execute(sql, name)[0]
+       Student.new(result[0], result[1], result[2])
+     end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
