2bc2da4e1746e9a261d461727485ca57967112bd
Stef Claus <stef.claus@gmail.com>
2019-01-13 15:28:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,12 @@ class Student
        student
      end
 
-    def self.new_from_db
+    def self.new_from_db(row)
+      new_student = Student.new  # self.new is the same as running Song.new
+      new_student.id = row[0]
+      new_student.name =  row[1]
+      new_student.grade = row[2]
+      new_student  # return the newly created instance
     end
 
     def self.find_by_name(name)
